9a6bb8c004f2831ef7d768b4d6847d59cec6510d
Jean-Pierre Ledos <jpledos@gmail.com>
2022-01-04 20:56:45 +0100

    jawsDb updated in manager.php

--- model/Manager.php
+++ model/Manager.php
@@ -2,20 +2,16 @@
 
 class Manager {
 
-   
-
-
-
     protected function dbConnect()
     { 
         $host = "ble5mmo2o5v9oouq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
         $username = "q47ssnjajcvnxdlk";
         $password = "be2q7wrfmmc0hugd";
-        $database = "un0clax84gfiuupj;
+        $database = "un0clax84gfiuupj";
         
         try
         {
-            $db = new PDO('mysql:host=$host;dbname=$database;charset=utf8', $username, $password);
+            $db = new PDO('mysql:host='.$host.';dbname='.$database.';charset=utf8', $username, $password);
             
             return $db;
         }
